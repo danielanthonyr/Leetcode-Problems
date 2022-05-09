0963a4ad9afa9eee5cbc6f9400d4cccabd78c7b7
Daniel-Anthony Romero <danielanthonyr@gmail.com>
2022-05-09 17:31:20 -0400

    Solved Max Root Path Sum problem

---
+++ Binary Trees/MaxRootToLeafPathSum.swift
@@ -0,0 +1,32 @@
+//
+//  MaxRootToLeafPathSum.swift
+//  Leetcode-Problems
+//
+//  Created by ryan on 2022-05-09.
+//
+
+import Foundation
+
+class Node {
+    var val: Int
+    var left: Node?
+    var right: Node?
+    
+    init(_ val: Int) {
+        self.val = val
+        left = nil
+        right = nil
+    }
+}
+
+func maxRootPathSum(root: Node?) -> Int {
+    if root == nil { return Int.min }
+    
+    if root?.left == nil && root?.right == nil {
+        return root!.val
+    }
+    
+    let currentPathSum = root!.val + max(maxRootPathSum(root: root?.left), maxRootPathSum(root: root?.right))
+    
+    return currentPathSum
+}
