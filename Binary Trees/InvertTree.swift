ad27a465f199604c3f8b48ff809f1ecb5cdb73f1
Daniel-Anthony Romero <danielanthonyr@gmail.com>
2022-05-10 15:57:23 -0400

    Solved invert binary tree

---
+++ Binary Trees/InvertTree.swift
@@ -0,0 +1,31 @@
+//
+//  InvertTree.swift
+//  Leetcode-Problems
+//
+//  Created by ryan on 2022-05-10.
+//
+
+import Foundation
+
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+    
+    init(_ val: Int) {
+        self.val = val
+        left = nil
+        right = nil
+    }
+}
+
+func invertTree(_ root: TreeNode?) -> TreeNode? {
+    if root == nil { return nil }
+    
+    let temp = root?.left
+    
+    root?.left = invertTree(root?.right)
+    root?.right = invertTree(temp)
+    
+    return root
+}
