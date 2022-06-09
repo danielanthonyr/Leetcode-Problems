59d663c43175ede3fed86f0a04d6c88ca0226cdc
Daniel-Anthony Romero <danielanthonyr@gmail.com>
2022-06-09 16:28:13 -0400

    Solve binary search problem

---
+++ Binary Search/BinarySearch.swift
@@ -0,0 +1,31 @@
+//
+//  BinarySearch.swift
+//  Leetcode-Problems
+//
+//  Created by daniel on 2022-06-09.
+//
+
+import Foundation
+
+func search(_ nums: [Int], _ target: Int) -> Int {
+    if nums.isEmpty { return -1 }
+    
+    var left = 0
+    var right = nums.count - 1
+    
+    while left < right {
+        let midpoint = (right) - left / 2
+        
+        if nums[midpoint] == target {
+            return midpoint
+        }
+        
+        if nums[midpoint] < target {
+            left = midpoint + 1
+        } else {
+            right = midpoint - 1
+        }
+    }
+    
+    return -1
+}
